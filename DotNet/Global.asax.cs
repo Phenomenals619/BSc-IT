38d8ccf438ca1d4b4172105653ab886cd4d618f0
Phenomenal <128923980+Phenomenals619@users.noreply.github.com>
2024-10-20 22:58:55 +0530

    Add files via upload

---
+++ DotNet/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Extraz
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Application.Lock();
+            Application["usercount"] = Convert.ToInt32(Application["usercount"])+1;
+            Application.UnLock();
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DotNet/UserVisit.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Extraz
+{
+    public partial class UserVisit : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //ViewState["x"] =Convert.ToInt32( ViewState["x"]) + 1;
+            //Label1.Text = ViewState["x"].ToString();
+
+            Application["x"] = Convert.ToInt32(Application["x"]) + 1;
+            Label1.Text = Application["x"].ToString();
+        }
+    }
+}
